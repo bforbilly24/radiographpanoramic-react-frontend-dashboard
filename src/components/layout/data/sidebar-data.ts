f456caebf3c3f739e8f1cc90ab21c5c6666cdf93
Halim <halimputra0701@gmail.com>
2025-05-12 00:36:22 +0700

    refactor(component): update nav group url

--- src/components/layout/data/sidebar-data.ts
+++ src/components/layout/data/sidebar-data.ts
@@ -55,8 +55,8 @@ export const sidebarData: SidebarData = {
           icon: IconLayoutDashboard,
         },
         {
-          title: 'Tasks',
-          url: '/tasks',
+          title: 'Radiographs',
+          url: '/radiographs',
           icon: IconChecklist,
         },
         {
