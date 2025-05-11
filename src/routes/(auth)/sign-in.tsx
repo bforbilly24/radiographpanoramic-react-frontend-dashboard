1b9c227ef1cb49b40203a756299fff1450135868
Halim <halimputra0701@gmail.com>
2025-05-12 00:59:31 +0700

    refactor(route): update route sign in, adding search schema

--- src/routes/(auth)/sign-in.tsx
+++ src/routes/(auth)/sign-in.tsx
@@ -1,6 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
-import SignIn from '@/features/auth/sign-in'
+// src/routes/(auth)/sign-in.tsx
+import { createFileRoute } from '@tanstack/react-router';
+import SignIn from '@/features/auth/sign-in';
+import { z } from 'zod';
+
+// Definisikan skema untuk parameter pencarian
+const searchSchema = z.object({
+  redirect: z.string().optional(), // redirect adalah string opsional
+});
 
 export const Route = createFileRoute('/(auth)/sign-in')({
   component: SignIn,
-})
+  validateSearch: searchSchema, // Validasi parameter pencarian
+});
